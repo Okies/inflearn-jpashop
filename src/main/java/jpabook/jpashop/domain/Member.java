0e41c4a75ef325e18899ffbfa1812f150129bc71
Okies <ja00800@nate.com>
2023-03-29 18:25:50 +0900

    test order domain

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -26,4 +26,8 @@ public class Member {
     public void setName(String name) {
         this.name = name;
     }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
